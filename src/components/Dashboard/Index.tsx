b03a675c917d95a9774a8bf7e1475d8581a2b401
Eridanus Sora <haolinaihui@Gmail.com>
2018-01-02 22:31:22 +0800

    improve layout

--- src/components/Dashboard/Index.tsx
+++ src/components/Dashboard/Index.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
+import { Row, Col, Layout } from 'antd';
 import Menu from '@/components/Menu/Index';
+import './Index.css';
+const { Header, Content } = Layout;
 
 class Dashboard extends React.Component {
     render() {
         return (
-            <Menu />
+            <div>
+                <Layout>
+                    <Header>
+                        <h2 className="logo">Shiny-Console</h2>
+                    </Header>
+                    <Content>
+                        <Row gutter={16}>
+                            <Col span={4}>
+                                <Menu />
+                            </Col>
+                            <Col span={20}>
+                                <div> Indeeeeeeeeeeeeeeeex </div>
+                            </Col>
+                        </Row>
+                    </Content>
+                </Layout>
+            </div>
         );
     }
 }
--- src/components/Menu/Index.tsx
+++ src/components/Menu/Index.tsx
@@ -3,23 +3,31 @@ import { Menu, Icon } from 'antd';
 const SubMenu = Menu.SubMenu;
 const MenuItemGroup = Menu.ItemGroup;
 
-class Sider extends React.Component {
+export interface Props {
+
+}
+
+class Sider extends React.Component<Props, {}> {
+  constructor(props: Props, context: object) {
+    super(props);
+    console.log(context);
+  }
   render() {
     return (
       <Menu
-        style={{ width: 256 }}
+        style={{ width: '100%' }}
         defaultSelectedKeys={['1']}
         defaultOpenKeys={['sub1']}
         mode="inline"
       >
-        <SubMenu key="sub1" title={<span><Icon type="mail" /><span>Navigation One</span></span>}>
-          <MenuItemGroup key="g1" title="Item 1">
-            <Menu.Item key="1">Option 1</Menu.Item>
-            <Menu.Item key="2">Option 2</Menu.Item>
+        <SubMenu key="sub1" title={<span><Icon type="mail" /><span>服务器</span></span>}>
+          <MenuItemGroup key="g1" title="TS处理">
+            <Menu.Item key="1">服务器节点管理</Menu.Item>
+            <Menu.Item key="2">API 密钥对管理</Menu.Item>
           </MenuItemGroup>
-          <MenuItemGroup key="g2" title="Item 2">
-            <Menu.Item key="3">Option 3</Menu.Item>
-            <Menu.Item key="4">Option 4</Menu.Item>
+          <MenuItemGroup key="g2" title="--">
+            <Menu.Item key="3">--</Menu.Item>
+            <Menu.Item key="4">--</Menu.Item>
           </MenuItemGroup>
         </SubMenu>
         <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>Navigation Two</span></span>}>
