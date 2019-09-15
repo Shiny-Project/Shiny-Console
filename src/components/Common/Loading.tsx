479529746e207f20d3ef1a8d098278939f5b26c5
Eridanus Sora <sora@sound.moe>
2019-09-15 16:43:16 +0800

    add component loading animation

--- src/components/Common/Loading.tsx
+++ src/components/Common/Loading.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ErrorPage from 'components/Common/Error';
+import './Loading.css';
 
 export interface LoadingProps {
     isLoading: boolean;
@@ -9,7 +10,11 @@ export interface LoadingProps {
 const Loading = ({ isLoading, error }: LoadingProps) => {
     // Handle the loading state
     if (isLoading) {
-        return <div>Loading...</div>;
+        return (
+            <div className="lds-dual-ring">
+                <span className="loading-text">组件加载中...</span>
+            </div>
+        );
     } else if (error) {
         return <ErrorPage message="组件加载出现错误" />;
     } else {
