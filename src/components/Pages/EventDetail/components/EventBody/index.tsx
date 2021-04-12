6ef683677738e6fabf065235dc9ff8fba934ec80
Eridanus Sora <sora@sound.moe>
2021-04-12 23:13:16 +0800

    fix styles

--- src/components/Pages/EventDetail/components/EventBody/index.tsx
+++ src/components/Pages/EventDetail/components/EventBody/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Row, Col } from 'antd';
 import Loading from "components/Common/Loading";
 import { ShinyEventDetail } from "types/dashboard";
 import EventTimeline from "./Timeline";
@@ -25,17 +26,17 @@ const EventBody: React.FC<Props> = (props) => {
     return (
         <div className="event-body">
             <h2 className="title">{eventDetail.event.data.title}</h2>
-            <div className="event-info-container">
-                <div className="event-desc">
+            <Row className="event-detail-container">
+                <Col xs={24} lg={24}>
                     <EventBasicInfo event={eventDetail} />
-                </div>
-                <div className="timeline-container">
+                </Col>
+                <Col xs={24} lg={8} className="timeline-container">
                     <EventTimeline event={eventDetail} />
-                </div>
-                <div className="event-content-container">
+                </Col>
+                <Col xs={24} lg={16} className="event-content-container">
                     <EventContent event={eventDetail} />
-                </div>
-            </div>
+                </Col>
+            </Row>
         </div>
     );
 };
