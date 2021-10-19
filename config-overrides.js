602f694a9d83d6c42e08b9a42b8b2c41f40ebdcb
Eridanus Sora <sora@sound.moe>
2021-10-20 00:02:12 +0800

    disable source map for production build

--- config-overrides.js
+++ config-overrides.js
@@ -1,27 +1,32 @@
-const tsImportPluginFactory = require('ts-import-plugin')
-const { getLoader, injectBabelPlugin } = require("react-app-rewired");
 const {
     override,
     fixBabelImports,
     addLessLoader,
     addBabelPlugin,
-    addWebpackAlias
+    addWebpackAlias,
 } = require("customize-cra");
-const path = require('path');
+const path = require("path");
 
 function resolve(dir) {
-    return path.join(__dirname, '.', dir)
+    return path.join(__dirname, ".", dir);
 }
 
 module.exports = override(
-    addBabelPlugin('syntax-dynamic-import'),
+    addBabelPlugin("syntax-dynamic-import"),
     fixBabelImports("import", {
-        libraryName: "antd", libraryDirectory: "es", style: true
+        libraryName: "antd",
+        libraryDirectory: "es",
+        style: true,
     }),
     addLessLoader({
         javascriptEnabled: true,
     }),
     addWebpackAlias({
-        '@': resolve('src')
-    })
+        "@": resolve("src"),
+    }),
+    (config) => {
+        config.devtool =
+            config.mode === "development" ? "cheap-module-source-map" : false;
+        return config;
+    }
 );
